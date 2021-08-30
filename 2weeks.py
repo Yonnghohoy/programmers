f26f3777874a69eacedb396298de228d447399db
yonnghohoy <akcnl5216@gmail.com>
2021-08-30 15:10:31 +0900

    programmers

--- 2weeks.py
+++ 2weeks.py
@@ -1,23 +1,21 @@
 def solution(scores):
-    answer = []
-    for j in range(len(scores)):
-        scores_list=[]
-        for i in range(len(scores)):
-            scores_list.append(scores[i][j])
-        if scores_list[j] in [max(scores_list), min(scores_list)]:
-            scores_list.remove(scores_list[j])
-        answer.append(sum(scores_list)/len(scores_list))
-    for i in range(len(answer)):
-        if answer[i] >= 90:
-            answer[i] = 'A'
-        elif answer[i] >= 80:
-            answer[i] = 'B'
-        elif answer[i] >= 70:
-            answer[i] = 'C'
-        elif answer[i] >= 50:
-            answer[i] = 'D'
-        else:
-            answer[i] = 'F'
-    return "".join(answer)
-
-print(solution([[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]))
+    answer = ''
+    lst = [[0 for i in range(len(scores))] for j in range(len(scores))]
+    for i in range(len(scores)):
+        for j in range(len(scores)):
+            lst[i][j] = scores[i][j]
+    
+    for i in range(len(lst)):
+        if lst[i][j] == max(lst[i]) and lst[i].count(max(lst[i])) == 1:
+            lst[i].pop(i)
+        elif lst[i][j] == min(lst[i]) and lst[i].count(max(lst[i])) == 1:
+            lst[i].pop(i)
+            
+        avg = sum(lst[i]) / len(lst)
+        if avg >= 90 : answer +=  "A"
+        elif avg >= 80 : answer +=  "B"
+        elif avg >= 70 : answer +=  "C"
+        elif avg >= 50 : answer +=  "D"
+        else: answer += "F"
+        
+    return answer
